6957660b9d113ee083305986af4e0124086b294c
emmanbobo <olawalealawode@gmail.com>
2025-04-10 15:43:20 +0100

    setup completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
 
+
 export default function Home() {
   return (
     <div>
+      <h1 className="text-3xl font-bold underline text-center">
+        Hello world!
+      </h1>
+      <p className="text-center">Welcome to the Drogo Store</p>
+      <p className="text-center">This is a sample text</p>
+      <p className="text-center">This is a sample text</p>
+      <p className="text-center">This is a sample text</p>
+      <p className="text-center">This is a sample text</p>
+      <p className="text-center">This is a sample text</p>
+      <p className="text-center">This is a sample text</p>
+      <p className="text-center">This is a sample text</p>
+      <p className="text-center">This is a sample text</p>
+      <p className="text-center">This is a sample text</p>
+      <p className="text-center">This is a sample text</p>
+      <p className="text-center">This is a sample text</p>  
+      <p className="text-center">This is a sample text</p>
+      <p className="text-center">This is a sample text</p>
+      <p className="text-center">This is a sample text</p>
+      <p className="text-center">This is a sample text</p>
+      <p className="text-center">This is a sample text</p>
 
+      <p className="text-center">This is a sample text</p>
     </div>
   );
 }
